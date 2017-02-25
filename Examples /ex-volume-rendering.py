239e5fb998ad1118ba0b5e6bb7400dbf7d453f04
Shrobon Biswas <shrobon.biswas@gmail.com>
2017-02-24 18:01:54 -0700

    Dataset Added

---
+++ Examples /ex-volume-rendering.py
@@ -0,0 +1,56 @@
+import vtk
+
+dir_ = r"CT"
+
+# Read data
+reader = vtk.vtkDICOMImageReader()
+reader.SetDirectoryName(dir_)
+reader.Update()
+
+# Create colour transfer function
+colorFunc = vtk.vtkColorTransferFunction()
+colorFunc.AddRGBPoint(-3024, 0.0, 0.0, 0.0)
+colorFunc.AddRGBPoint(-77, 0.54902, 0.25098, 0.14902)
+colorFunc.AddRGBPoint(94, 0.882353, 0.603922, 0.290196)
+colorFunc.AddRGBPoint(179, 1, 0.937033, 0.954531)
+colorFunc.AddRGBPoint(260, 0.615686, 0, 0)
+colorFunc.AddRGBPoint(3071, 0.827451, 0.658824, 1)
+
+# Create opacity transfer function
+alphaChannelFunc = vtk.vtkPiecewiseFunction()
+alphaChannelFunc.AddPoint(-3024, 0.0)
+alphaChannelFunc.AddPoint(-77, 0.0)
+alphaChannelFunc.AddPoint(94, 0.29)
+alphaChannelFunc.AddPoint(179, 0.55)
+alphaChannelFunc.AddPoint(260, 0.84)
+alphaChannelFunc.AddPoint(3071, 0.875)
+
+# Instantiate necessary classes and create VTK pipeline
+volume = vtk.vtkVolume()
+ren = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren)
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+renWin.SetSize(800,800)
+
+# Define volume mapper
+volumeMapper = vtk.vtkSmartVolumeMapper()  
+volumeMapper.SetInputConnection(reader.GetOutputPort())
+
+# Define volume properties
+volumeProperty = vtk.vtkVolumeProperty()
+volumeProperty.SetScalarOpacity(alphaChannelFunc)
+volumeProperty.SetColor(colorFunc)
+volumeProperty.ShadeOn()
+
+# Set the mapper and volume properties
+volume.SetMapper(volumeMapper)
+volume.SetProperty(volumeProperty)  
+
+# Add the volume to the renderer
+ren.AddVolume(volume)
+
+# Render the scene
+renWin.Render()
+iren.Start()
