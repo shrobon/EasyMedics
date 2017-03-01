cb0cfda8851082c66bcc884e0c21161055b3be08
Shrobon Biswas <shrobon.biswas@gmail.com>
2017-03-01 07:27:19 -0700

    FInally Done with colours

--- Practice/IntegratingVtkWithTkinter/vtk2.py
+++ Practice/IntegratingVtkWithTkinter/vtk2.py
@@ -11,45 +11,38 @@ import vtk.tk.vtkTkRenderWidget
 
 
 
-val = 0.2
+#val = 0.0
 root = Tk()
 
 #Function for accessing the slider values
 def show_values(event):
-    val = float(slider.get()/255)
+    val = float("{0:.2f}".format(slider.get()/255))
     label.config(text=str(val))
     
-
+    
 #For naming the title bar
 root.title("Hello From Tkinter")
 frame = Frame(root)
 frame.pack(side='top')
 
-#Slider to change the background colour of the renderer
-slider = Scale(root,from_=0, to_=255, orient = HORIZONTAL,command=show_values)
-#slider = Scale(root,from_=0, to_=255, orient = HORIZONTAL)
-slider.pack()
-Button(root,text="Show").pack()
+
+#Button(root,text="Show").pack()
 label = Label(root,fg='blue')
 label.pack()
 
 
 cube = vtk.vtkCubeSource()
-cube.Update()
+cube.Modified()
 mapper = vtk.vtkPolyDataMapper()
 actor = vtk.vtkActor()
 
 
 mapper.SetInputConnection(cube.GetOutputPort())
 actor.SetMapper(mapper)
-
-
 render = vtk.vtkRenderer()
-#render.SetBackground( 0.329412, 0.34902, 0.427451 )
+#render.SetBackground(val,val,val)
 render.AddActor(actor)
 
-print val 
-render.SetBackground(val,val,val)
 
 renWindow = vtk.vtkRenderWindow()
 renWindow.AddRenderer(render)
@@ -57,7 +50,24 @@ renWindow.AddRenderer(render)
 renWinInteract = vtkTkRenderWindowInteractor(root,rw=renWindow, width=400, height=400)                   
 renWinInteract.Initialize()
 renWinInteract.pack(side='top', fill='both', expand=1)
-renWinInteract.Start()
 
-renWindow.Render()
+
+def change_background(num):
+    val = float("{0:.2f}".format(slider.get()/255))
+    label.config(text=str(val))
+    render.SetBackground(val,val,val)
+    renWindow.Render()
+
+#Slider to change the background colour of the renderer
+slider = Scale(root,from_=0, to_=255, orient = HORIZONTAL,command=change_background,label="BG_COL")
+#slider = Scale(root,from_=0, to_=255, orient = HORIZONTAL)
+slider.set(0)
+slider.pack()
+
+
+
+
+
+
+renWinInteract.Start()
 root.mainloop()
